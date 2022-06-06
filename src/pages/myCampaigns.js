fd9f6c2821f3825f74e1001d3dcc8854a82f5261
geesimon <geesimon@hotmail.com>
2022-06-06 23:36:35 +0800

    Fix id problem when deploy to OSS

--- src/pages/myCampaigns.js
+++ src/pages/myCampaigns.js
@@ -51,7 +51,7 @@ const MyCampaigns = () => {
                   <td>{c.id}</td>
                   <td>{c.name}</td>
                   <td>{c.balance}</td>
-                  <td><Button href={"../campaign?id="+c.id}>Details</Button></td>
+                  <td><Button href={"../campaign/?id="+c.id}>Details</Button></td>
                 </tr>
               )
             })
